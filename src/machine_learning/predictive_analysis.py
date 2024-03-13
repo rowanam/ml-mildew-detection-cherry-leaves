a332506ab2436de0d4934e4cf48fa8d144f75132
rowanam <rowanamaas@gmail.com>
2024-03-13 19:38:06 +0000

    Add libraries used, project introduction and credits

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.express as px
 from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
